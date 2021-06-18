ee2225231111aa8fe8871bb79e583a81e0d5ec63
romish-maker <romish2020@list.ru>
2021-06-18 16:07:16 +0600

    learn and add use.Memo

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 
 export default {
     title: 'react.memo demo'
@@ -10,26 +10,28 @@ const NewMessageCounter = (props: { count: number }) => {
 }
 
 const UsersSecret = (props: { users: Array<string> }) => {
-    console.log("USERS")
-    return <div>{
-        props.users.map((u, i) => <div key={i}>{u}</div>)
-    }</div>
+    console.log("UsersSecret");
+    return <div>
+        {
+            props.users.map((u, i) => <div key={i}>{u}</div>)
+        }
+    </div>
 }
 const Users = React.memo(UsersSecret)
 
-
 export const Example1 = () => {
-    console.log("Example1")
-    const [counter, setCounter] = useState(0);
-    const [users, setUsers] = useState(["Romish", "Shaha", "Behruz", "Katya"]);
+    const [count,setCount] = useState(0)
+    const [users,setUsers] = useState(['Romish', 'Beha', 'Rama'])
     const addUser = () => {
-        const copyUsers = [...users,"Sveta " + new Date().getTime() ];
-        setUsers(copyUsers);
+        const copyUsers =  [...users, 'Rinat ' + new Date().getTime() ]
+        setUsers(copyUsers)
     }
-    return <div>
-        <button onClick={() => setCounter(counter + 1)}>+</button>
-        <button onClick={addUser}>add user</button>
-        <NewMessageCounter count={counter}/>
-        <Users users={users}/>
-    </div>
+    return (
+        <>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={addUser}>add user</button>
+            <NewMessageCounter count={count}/>
+            <Users users={users}/>
+        </>
+    )
 }
---
+++ src/stories/UseMemo.stories.tsx
@@ -0,0 +1,117 @@
+import * as React from "react";
+import {useMemo, useState} from "react";
+
+export default {
+    title: 'use.memo demo'
+}
+
+export const DifficultCountingExample = () => {
+    const [a, setA] = useState<number>(6)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for(let i = 1; i <= a; i++) {
+            let fake = 0
+            while(fake < 100000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA
+    }, [a])
+
+    for(let i = 1; i <= b; i++) {
+        resultB = resultB * i
+    }
+
+    return <>
+        <input value={a} onChange={ (e) => setA(+e.currentTarget.value)} />
+        <input value={b} onChange={ (e) => setB(+e.currentTarget.value)} />
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+        </>
+}
+
+export const HelpsForReactMemoExample = () => {
+    const [a, setA] = useState<number>(6)
+    const [b, setB] = useState<number>(5)
+
+    let resultA = 1;
+    let resultB = 1;
+
+    resultA = useMemo(() => {
+        let tempResultA = 1;
+        for(let i = 1; i <= a; i++) {
+            let fake = 0
+            while(fake < 100000000) {
+                fake++
+                const fakeValue = Math.random()
+            }
+            tempResultA = tempResultA * i
+        }
+        return tempResultA
+    }, [a])
+
+    for(let i = 1; i <= b; i++) {
+        resultB = resultB * i
+    }
+
+    return <>
+        <input value={a} onChange={ (e) => setA(+e.currentTarget.value)} />
+        <input value={b} onChange={ (e) => setB(+e.currentTarget.value)} />
+        <hr/>
+        <div>
+            Result for a: {resultA}
+        </div>
+        <div>
+            Result for b: {resultB}
+        </div>
+    </>
+}
+
+const NewMessageCounter = (props: { count: number }) => {
+    return <div>{props.count}</div>
+}
+
+const UsersSecret = (props: { users: Array<string> }) => {
+    console.log("UsersSecret");
+    return <div>
+        {
+            props.users.map((u, i) => <div key={i}>{u}</div>)
+        }
+    </div>
+}
+const Users = React.memo(UsersSecret)
+
+export const HelpsForReactMemo = () => {
+    console.log("HelpsForReactMemo")
+    const [count,setCount] = useState(0)
+    const [users,setUsers] = useState(['Romish', 'Beha', 'Rama'])
+    const newArray = useMemo(() => {
+        return users.filter(u => u.toLowerCase().indexOf("a") > -1)
+    }, [users])
+
+    const addUser = () => {
+        const copyUser = [...users, 'Shaha ' + new Date().getTime()]
+        setUsers(copyUser)
+    }
+
+    return (
+        <>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={addUser}>addUser</button>
+            <NewMessageCounter count={count}/>
+            <Users users={newArray}/>
+        </>
+    )
+}
